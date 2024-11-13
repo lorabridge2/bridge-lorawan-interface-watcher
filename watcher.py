26022760419e2fa0bcb3c9a5c039dc965e4539f9
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2024-11-13 22:16:16 +0100

    dont restart manually stopped container

--- watcher.py
+++ watcher.py
@@ -4,7 +4,7 @@ import time
 client = docker.from_env()
 while True:
     c = client.containers.get("bridge-lorawan-interface-1")
-    if c.status == "exited":
+    if c.status == "exited" and c.attrs["State"]["ExitCode"] != 137:
         c.start()
         print("container started")
     time.sleep(2)
