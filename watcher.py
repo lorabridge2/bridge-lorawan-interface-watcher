efbb9e1db8964621633cface55305b1882ae438d
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2024-11-14 13:12:08 +0100

    catch error when device was removed

--- watcher.py
+++ watcher.py
@@ -5,6 +5,16 @@ client = docker.from_env()
 while True:
     c = client.containers.get("bridge-lorawan-interface-1")
     if c.status == "exited" and c.attrs["State"]["ExitCode"] != 137:
-        c.start()
-        print("container started")
+        try:
+            c.start()
+            print("container started")
+        except docker.errors.APIError as err:
+            if (
+                err.is_server_error()
+                and "error gathering device information while adding custom device"
+                in str(err)
+            ):
+                print(err)
+            else:
+                raise err
     time.sleep(2)
